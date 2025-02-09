5a797f723493d8fe9411a7fb1e6eda5d7d00180f
bulsati <akhmetsharif@gmail.com>
2025-02-09 19:08:21 +0500

    Первый коммит

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color:'white'}}>Open up App.js to start working on your app! Im sure u will getting bw excited about this
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'red',
+  },
+});
